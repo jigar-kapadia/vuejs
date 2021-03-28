4e7aea33e6dc2a0365749494c02c813a15164cbf
Jigar Kapadia <jigark00@gmail.com>
2021-03-28 13:29:43 +0530

    code added

--- without-cli/index.js
+++ without-cli/index.js
@@ -1,10 +1,24 @@
+
 var app = new Vue({
     el: "#app",
     data: {
         product : "Boots",
         message : "You have hovered me.",
         message1 : "sunshine",
-        newMessage : ''
+        newMessage : '',
+        rawHtml : '<span style="color: red;">This should be red</span>',
+        users : [],
+        showUsers : true
+    },
+    created() {
+        fetch('https://jsonplaceholder.typicode.com/users')
+        .then(x => x.json())
+        .then(response =>
+            { console.log(response);
+            this.users = response});
+    },
+    mounted() {
+        
     },
     computed: {
         reversedMessage1 : function (){
@@ -13,7 +27,17 @@ var app = new Vue({
     },
     methods: {
         changeMessage(){
+            if(this.newMessage === '' || this.newMessage === null){
+                throw new Error('New Message is null or empty. please enter some value in textbox.');
+        }
+        else
             this.message1 = this.newMessage;
+        },
+        throwErr(){
+            throw new Error("Custom exception thrown");
+        },
+        someClickEvent(){
+            alert('Clicked Me');
         }
     },
 });
@@ -21,4 +45,14 @@ var app = new Vue({
 app.$watch('message1', (newVal, oldVal) =>{
     console.log(`The value of message1 changed from ${oldVal} to ${newVal}`);
 });
+
+Vue.config.errorHandler = (err, vm, info) => {
+    // handle error
+    let data = `Error occured \n Error message : ${err} \n Reason : ${vm}`;
+    // let fso = new ActiveXObject('Scripting.FileSystemObject');
+    // var fh = fso.OpenTextFile("errorlogs.txt", 8, false, 0);
+    // fh.WriteLine(d1 + ',' + d2);
+    // fh.Close();
+    console.log(err);
+}
 
